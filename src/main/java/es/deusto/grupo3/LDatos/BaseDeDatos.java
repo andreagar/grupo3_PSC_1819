af3c8fc4931f0cb2bb2fb53667ff177e657bfd31
Ainhoa <euskadi612>
2018-10-08 10:59:11 +0200

    Primeros pasos de visualización de la vista principal

--- src/main/java/es/deusto/grupo3/LDatos/BaseDeDatos.java
+++ src/main/java/es/deusto/grupo3/LDatos/BaseDeDatos.java
@@ -1,5 +1,45 @@
 package es.deusto.grupo3.LDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
 public class BaseDeDatos {
 
+	private static Connection connection = null;
+	private static Statement statement = null;
+	
+	/** Inicializa una BD SQLITE y devuelve una conexion con ella. Debe llamarse a este 
+	 * metodo antes que ningun otro, y debe devolver no null para poder seguir trabajando con la BD.
+	 * @param nombreBD	Nombre de fichero de la base de datos
+	 * @return	Conexion con la base de datos indicada. Si hay algun error, se devuelve null
+	 */
+	public static Connection initBD( String nombreBD ) {
+		try {
+		    Class.forName("org.sqlite.JDBC"); //.jar
+		    connection = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
+			statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // poner timeout 30 msg. 
+			//TIMEOUT: Si estamos esperando una respuesta y si no nos dan en 30 mseg (en este caso), dejamos de esperar.
+			JOptionPane.showMessageDialog( null, "Ondo!! Nombre: " + nombreBD + " / DATU BASEA KONEKTATU DA" , "BIEN", JOptionPane.INFORMATION_MESSAGE);
+		    return connection;
+		} catch (ClassNotFoundException | SQLException e) {
+			JOptionPane.showMessageDialog( null, "Error!! Nombre: " + nombreBD + " / no se ha conectado la base de datos" , "ERROR", JOptionPane.ERROR_MESSAGE );
+			System.out.println( "Error!! Nombre: " + nombreBD + " / Contrasenya incorreta");
+			return null;
+		}
+	}
+	
+	/** Cierra la conexion con la Base de Datos*/
+	public static void close() {
+		try {
+			statement.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/main/java/es/deusto/grupo3/LPresentacion/vistaPrincipal.java
@@ -0,0 +1,82 @@
+package es.deusto.grupo3.LPresentacion;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import es.deusto.grupo3.LDatos.BaseDeDatos;
+
+public class vistaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JButton btnLogIn;
+	private JButton btnRegistrarse;
+	
+	public vistaPrincipal()
+	{
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 474, 393);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnLogIn = new JButton("LOG IN");
+		btnLogIn.setFont(new Font("Stencil", Font.BOLD, 18));
+		btnLogIn.setBounds(10, 141, 438, 75);
+		contentPane.add(btnLogIn);
+		//btnLogIn.addActionListener(this);
+		
+		btnRegistrarse = new JButton("REGISTRARSE");
+		btnRegistrarse.setFont(new Font("Stencil", Font.BOLD, 18));
+		btnRegistrarse.setBounds(10, 227, 438, 75);
+		contentPane.add(btnRegistrarse);
+		
+		JLabel lblNewLabel = new JLabel("HyraCar");
+		lblNewLabel.setFont(new Font("Stencil", Font.BOLD, 24));
+		lblNewLabel.setBounds(153, 50, 212, 41);
+		contentPane.add(lblNewLabel);
+		//btnRegistrarse.addActionListener(this);
+		
+		
+		addWindowListener( new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				
+				BaseDeDatos.close();
+				
+			}
+		});
+	}
+	
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//
+//		if (e.getSource() == btnRegistrarse){
+//			
+//			registrarse registrarse = new registrarse();
+//			registrarse.setVisible(true);
+//			
+//		}
+//		
+//		if (e.getSource() == btnLogIn){
+//			
+//
+//			logIn logIn = new logIn();
+//			logIn.setVisible(true);
+//			dispose();
+//			
+//		}
+//		
+//	}
+}
