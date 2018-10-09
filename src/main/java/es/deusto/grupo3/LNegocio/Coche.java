0756d1f68bc719abf5a13eed6207cdb5b444c1c6
Oscar <Oscar>
2018-10-09 17:46:34 +0200

    creacion de clase coche

---
+++ src/main/java/es/deusto/grupo3/LNegocio/Coche.java
@@ -0,0 +1,36 @@
+package es.deusto.grupo3.LNegocio;
+
+public class Coche {
+
+	String marca, modelo, anyo;
+
+	public Coche(String marca, String modelo, String anyo){
+		
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getAnyo() {
+		return anyo;
+	}
+
+	public void setAnyo(String anyo) {
+		this.anyo = anyo;
+	}
+
+	
+}
