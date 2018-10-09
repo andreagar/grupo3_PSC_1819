87de7aeb4d903f28aec6451f657de725cbb4fb57
Oscar <Oscar>
2018-10-09 19:21:54 +0200

    gestorCoche + insertCoches desde el main

--- src/main/java/es/deusto/grupo3/App.java
+++ src/main/java/es/deusto/grupo3/App.java
@@ -1,6 +1,8 @@
 package es.deusto.grupo3;
 
 import es.deusto.grupo3.LDatos.BaseDeDatos;
+import es.deusto.grupo3.LNegocio.GestorCoche;
+import es.deusto.grupo3.LNegocio.GestorUsuario;
 import es.deusto.grupo3.LPresentacion.vistaPrincipal;
 
 /**
@@ -15,6 +17,16 @@ public class App
 		BaseDeDatos.crearTablaBDUsuario();
 		BaseDeDatos.crearTablaBDCoche();
 		
+		GestorCoche coche;
+		
+		coche = new GestorCoche("Audi", "A7", "1234ABC");
+		coche.anyadirFilaATablaCoche(BaseDeDatos.getStatement(), "1234ABC");
+		
+		coche = new GestorCoche("Audi", "Q3", "9999BBB");
+		coche.anyadirFilaATablaCoche(BaseDeDatos.getStatement(), "9999BBB");
+		
+		coche = new GestorCoche("Ford", "Kuga", "5678HHH");
+		coche.anyadirFilaATablaCoche(BaseDeDatos.getStatement(), "5678HHH");
 
 		//menu de log in y registrarse
 		vistaPrincipal frame = new vistaPrincipal();
---
+++ src/main/java/es/deusto/grupo3/LNegocio/GestorCoche.java
@@ -0,0 +1,79 @@
+package es.deusto.grupo3.LNegocio;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import es.deusto.grupo3.LDatos.BaseDeDatos;
+
+public class GestorCoche {
+
+	String marca;
+	String modelo;
+	String matricula;
+		
+	public GestorCoche (String marca, String modelo, String matricula){
+		this.marca = marca;
+		this.modelo = modelo;
+		this.matricula = matricula;
+		
+	}
+		
+	/** Comprueba si un coche ya esta en la tabla COCHE de BD,considerando la trayectoria completa del disco como informacion clave.
+	 * @param st	Sentencia ya abierta de base de datos
+	 * @return	true si el usuario ya esta en la tabla, false en caso contrario
+	 */
+	public boolean chequearYaEnTabla( Statement st, String matricula ) {
+		//SELECT
+			try {
+
+				String sentSQL = "select * from COCHE where (matricula = '" + matricula + "')";
+				System.out.println( sentSQL ); 
+				
+				ResultSet rs = st.executeQuery( sentSQL );
+				
+				if (rs.next()) {  // Normalmente se recorre con un while, pero aqui solo hay que ver si ya existe
+					rs.close();
+					JOptionPane.showMessageDialog(null, "El coche ya existe, prueba con otro","Mensaje de error",JOptionPane.ERROR_MESSAGE);
+					return true;
+				}
+				return false;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+			
+	/** AÃ±ade un usuario a la tabla USUARIO de BD, 
+	 * que debe estar abierta y tener el formato y los nombres de campos apropiados: (int idUsuario, Sting nombre, String password)
+	 * Usa la sentencia INSERT de SQL.
+	 * @param st	Sentencia ya abierta de Base de Datos (con la estructura de tabla correspondiente al coche)
+	 * @return	true si la inserciÃ³n es correcta, false en caso contrario
+	 */
+	public boolean anyadirFilaATablaCoche( Statement st, String matricula ) {
+	//INSERT
+
+		if (chequearYaEnTabla(st, matricula) == false) {  // Si esta ya en la tabla
+			// Insercion normal
+			try {
+				String sentSQL = "insert into COCHE values(" + "'" + marca + "', " + "'" + modelo + "', " +"'" + matricula + "')"; 
+				System.out.println( sentSQL );  // (Quitar) para ver lo que se hace
+				int val = st.executeUpdate( sentSQL );
+				if (val!=1) return false;  // Se tiene que aÃ±adir 1 - error si no
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return false;
+		
+	}
+	
+}
+	
+	
