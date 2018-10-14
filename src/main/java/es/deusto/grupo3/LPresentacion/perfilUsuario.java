9abb44c8c06d3fcb5fa5d00441041de65a11d2ae
Oscar <Oscar>
2018-10-14 22:59:23 +0200

    nueva ventana perfil usuario

---
+++ src/main/java/es/deusto/grupo3/LPresentacion/perfilUsuario.java
@@ -0,0 +1,76 @@
+package es.deusto.grupo3.LPresentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+public class perfilUsuario extends JFrame {
+
+	private JPanel contentPane;
+	private JPasswordField passwordOld;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					perfilUsuario frame = new perfilUsuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public perfilUsuario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		passwordOld = new JPasswordField();
+		passwordOld.setBounds(140, 70, 131, 20);
+		contentPane.add(passwordOld);
+		
+		JLabel lblContraseaActual = new JLabel("Contraseña actual");
+		lblContraseaActual.setHorizontalAlignment(SwingConstants.CENTER);
+		lblContraseaActual.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContraseaActual.setBounds(129, 30, 148, 22);
+		contentPane.add(lblContraseaActual);
+		
+		JLabel lblNewPassword = new JLabel("Contraseña nueva");
+		lblNewPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewPassword.setBounds(129, 112, 148, 22);
+		contentPane.add(lblNewPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(140, 145, 131, 20);
+		contentPane.add(passwordField);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(10, 228, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.setBounds(335, 228, 89, 23);
+		contentPane.add(btnGuardar);
+	}
+}
