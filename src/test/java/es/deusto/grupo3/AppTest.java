d40b56c5dd9250c324e5d231e8badcfb94876889
andreagar <andrea.gar@opendeusto.es>
2018-10-19 00:15:35 +0200

    Anyadido JUnit

--- src/test/java/es/deusto/grupo3/AppTest.java
+++ src/test/java/es/deusto/grupo3/AppTest.java
@@ -2,17 +2,43 @@ package es.deusto.grupo3;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import es.deusto.grupo3.LDatos.BaseDeDatos;
+import es.deusto.grupo3.LNegocio.GestorCoche;
+import es.deusto.grupo3.LNegocio.GestorUsuario;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+    
+    @Before
+	public void setUp() throws Exception {
+		BaseDeDatos.initBD("nuestroBD.db");
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		BaseDeDatos.close();
+	}
+    
     @Test
-    public void shouldAnswerWithTrue(){
-        assertTrue( true );
-    }
+	public void testchequearTablaCoche() {
+    	GestorCoche gestor = new GestorCoche("Audi", "A7", "1234ABC");
+    	boolean prueba = gestor.chequearYaEnTabla(BaseDeDatos.getStatement(), "1234ABC");
+    	assertTrue(prueba);		
+	}
+    
+    @Test
+   	public void testchequearTablaUsuario() {
+    	GestorUsuario gestor = new GestorUsuario("andrea","andrea");
+    	boolean prueba = gestor.chequearYaEnTabla(BaseDeDatos.getStatement(), "andrea");
+       	assertTrue(prueba);		
+   	}
 }
