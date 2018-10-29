a7bcab644a746d548de8dc8712cfa699bdf3a98b
Oscar <oscar.bonet@opendeusto.es>
2018-10-29 09:26:52 +0100

    Clase moto PROYGRUPO3PSC1819-83

--- src/main/java/es/deusto/grupo3/LNegocio/Moto.java
+++ src/main/java/es/deusto/grupo3/LNegocio/Moto.java
@@ -1,5 +1,60 @@
 package es.deusto.grupo3.LNegocio;
 
 public class Moto {
+	
+	String marca, modelo, matricula;
+	boolean alquilado, comprado, averiado;
+	
+	public Moto(String marca, String modelo, String matricula, boolean alquilado, boolean comprado, boolean averiado) {
+	}
 
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public boolean isAlquilado() {
+		return alquilado;
+	}
+
+	public void setAlquilado(boolean alquilado) {
+		this.alquilado = alquilado;
+	}
+
+	public boolean isComprado() {
+		return comprado;
+	}
+
+	public void setComprado(boolean comprado) {
+		this.comprado = comprado;
+	}
+
+	public boolean isAveriado() {
+		return averiado;
+	}
+
+	public void setAveriado(boolean averiado) {
+		this.averiado = averiado;
+	}
+	
+	
 }
