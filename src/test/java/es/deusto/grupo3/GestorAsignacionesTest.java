30ea1ccd5a5609f21a0ac2e4e370b93a573cee76
Ainhoa <ainhoaei@hotmail.com>
2018-11-19 18:17:17 +0100

    Testing PROYGRUPO3PSC1819-63 Cancelación de alquileres

--- src/test/java/es/deusto/grupo3/GestorAsignacionesTest.java
+++ src/test/java/es/deusto/grupo3/GestorAsignacionesTest.java
@@ -42,9 +42,35 @@ public class GestorAsignacionesTest {
 		ArrayList<Asignaciones> asigArray = new ArrayList<Asignaciones>();
 		gestorCoche.AlquilarVehiculoUsuario(BaseDeDatos.getStatement(), asig);
 		asigArray = gestor.getUsuarioHistorial(BaseDeDatos.getStatement(), "Ainhoa");
+		int comprobacion = 0;
+		
+		assertNotSame(asigArray.size(), comprobacion);
+	}
+
+	@Test
+	public void GetArrayUsuarioAlquilados(){
+		ArrayList<Asignaciones> asigArray = new ArrayList<Asignaciones>();
+		asigArray = gestor.GetArrayUsuarioAlquilados(BaseDeDatos.getStatement(), "Ainhoa");
 		boolean comprobacion = true;
 		
 		assertSame(asigArray.get(1).getAlquilado(), comprobacion);
 	}
-
+	
+	@Test
+	public void CancelarAlquiler(){
+	
+		//CONTAR ASIGNACIONES
+		Asignaciones asigCambio = new Asignaciones ("Ainhoa", "9999BBB", true, false, false, 1);
+		gestorCoche.AlquilarVehiculoUsuario(BaseDeDatos.getStatement(), asigCambio);
+		ArrayList<Asignaciones> asignaciones1 = new ArrayList<Asignaciones>();
+		asignaciones1 = gestor.GetArrayUsuarioAlquilados(BaseDeDatos.getStatement(), "Ainhoa");
+		
+		//CONTAR ASIGNACIONES DESPUÉS DE BORRAR UNA ASIGNACIÓN
+		gestor.CancelarAlquiler(BaseDeDatos.getStatement(), "9999BBB", "Ainhoa", "Coche");
+		ArrayList<Asignaciones> asignaciones2 = new ArrayList<Asignaciones>();
+		asignaciones2 = gestor.GetArrayUsuarioAlquilados(BaseDeDatos.getStatement(), "Ainhoa");
+		
+		//COMPARAR TAMAÑOS DE LOS ARRAYS
+		assertSame((asignaciones1.size()-1), asignaciones2.size());
+	}
 }
